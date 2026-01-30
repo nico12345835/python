cfc3885c47580f3051a8f48e2fe72605d8100166
nico12345835 <nicolasleonard270@gmail.com>
2026-01-30 23:31:53 +0100

    Create programmeracine.py

---
+++ programmeracine.py
@@ -0,0 +1,26 @@
+from math import sqrt #on importe le module permettant de calculer des racines carrées
+
+print("Bonjour et bienvenue dans ce programme de calcul des racines d'une fonction du deuxième degré.")#on affiche un message de bienvenue
+print("Dans l'équation y=ax²+bx+c,")#on précise le contexte et le nom des variables
+
+a=input("Quelle est la valeur de a? ")#on récupère la valeur de a et on la stocke dans la variable a
+b=input("Quelle est la valeur de b? ")#même chose pour b
+c=input("Quelle est la valeur de c? ")#même chose pour c
+
+a=float(a) #on converti en un nombre décimal
+b=float(b) #même chose
+c=float(c) #même chose
+
+#################################
+#continue le code à partir d'ici#
+#################################
+delta=b**2-4*a*c
+if delta > 0:
+    x1=-b-sqrt(delta)/2*a
+    x2=-b+sqrt(delta)/2*a
+    x1=round(x1 , 5)
+    x2=round(x2 , 5)
+    print(f"x1 = {x1}")
+    print(f"x2 = {x2}")
+else:
+	print("il n'y a pas de racines a cette fonction")
