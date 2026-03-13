013f8c04bac25f90da15b357c665af58fd6c8d86
nico12345835 <nicolasleonard270@gmail.com>
2026-03-13 18:59:10 +0100

    Add files via upload

---
+++ pendu (1).py
@@ -0,0 +1,29 @@
+mot="anniversaire"
+list_lettres_mot=[]
+#test update
+#on definit la liste de lettres du mot
+for lettre in mot:
+    if lettre not in list_lettres_mot:
+        list_lettres_mot.append(lettre)
+tentatives_autorisees=len(list_lettres_mot)+3
+    
+list_propositions=[]
+for i in range(tentatives_autorisees):
+    for lettre in mot:
+        if lettre in list_propositions :
+            print( lettre,end="")
+        else:
+            print(" _",end="")
+    print("")
+    proposition=input("Faites une proposition de lettre ")
+    list_propositions.append(proposition)
+    if len(proposition)>1:
+        if proposition==mot:
+            print("tiéé un tigre")
+            print(f"le mot etait bien : {mot}")
+            break
+    if set(list_propositions).intersection(set(list_lettres_mot))==set(list_lettres_mot):
+        print("tiééé un tigre")
+        print(f"le mot etait bien : {mot}")
+        break
+    
